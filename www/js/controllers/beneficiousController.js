2e1c0d1048ffe41f3cd4c507200301420013f476
prajapatihardik <prajapatihardik1212@gmail.com>
2016-12-22 18:56:13 +0530

    category API call integrated, coupon sevice created

--- www/js/controllers/beneficiousController.js
+++ www/js/controllers/beneficiousController.js
@@ -1,4 +1,23 @@
-angular.module('starter').controller('beneficiousController', function($scope, $state, $window) {
+angular.module('starter').controller('beneficiousController', function($scope, $state, $window, CouponService) {
+
+  $scope.$on('$ionicView.enter', function(ev) {
+    if (ev.targetScope !== $scope)
+      return;
+
+    CouponService.getAllCategories().then(function(response) {
+      $scope.categories = [];
+      if (response.success = "true") {
+        console.log('yes true');
+          $scope.categories = response.data;
+          console.log('$scope.categories : ' +  angular.toJson(response.data, ' ') );
+      }
+    }).catch(function(error) {
+        var error = JSON.parse(error);
+        IonicPopupService.alert("ERROR!!", error.msg);
+        console.log('error : ' + angular.toJson(error, ' '));
+    });
+
+  });
 
 
   $scope.goBack = function() {
---
+++ www/js/services/CouponService.js
@@ -0,0 +1,69 @@
+angular.module('starter')
+
+.service('CouponService', ["$q", "$http", function($q, $http) {
+
+    this.getAllCategories = function() {
+        var def = $q.defer();
+
+        var url = AppSettings.url + 'coupons/categories';
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        $http({
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            }
+        }).success(function(body, status, headers, config) {
+            console.log("body : " + JSON.stringify(body));
+            def.resolve(body);
+        }).
+        error(function(error, status, headers, config) {
+            console.log("Error : " + JSON.stringify(error));
+            def.reject(JSON.stringify(error));
+        });
+
+        return def.promise;
+    };
+
+    this.getSubCategories = function(catId) {
+        var def = $q.defer();
+
+        var url = AppSettings.url + 'coupons/subcategories/'+catId;
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        $http({
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            }
+        }).success(function(body, status, headers, config) {
+            console.log(" body : " + JSON.stringify(body));
+            def.resolve(JSON.stringify(body));
+        }).
+        error(function(error, status, headers, config) {
+            console.log("Error : " + JSON.stringify(error));
+            def.reject(JSON.stringify(error));
+        });
+
+        return def.promise;
+    };
+
+}]);
