059ccf86820e01aadda1149df290be8ed4e07445
prajapatihardik <prajapatihardik1212@gmail.com>
2016-12-24 12:14:22 +0530

    circular Api  calling

--- www/js/controllers/noticasController.js
+++ www/js/controllers/noticasController.js
@@ -1,4 +1,4 @@
-angular.module('starter').controller('noticasController', function($scope, $state, $window) {
+angular.module('starter').controller('noticasController', function($scope, $state, $window,$sce, CouponService) {
 
   $scope.goBack = function() {
     $window.history.back();
@@ -11,4 +11,28 @@ angular.module('starter').controller('noticasController', function($scope, $stat
   $scope.File_Id = 'myId';
 
 
+  $scope.$on('$ionicView.enter', function(ev) {
+    if (ev.targetScope !== $scope)
+      return;
+  $scope.link='http://www.cbu.edu.zm/downloads/pdf-sample.pdf';
+
+    CouponService.getAllCirculars().then(function(response) {
+
+      if (response.success = "true") {
+        $scope.circulars = response.data;
+        var file = new Blob([response.data.path], {type: 'application/pdf'});
+        var fileURL = URL.createObjectURL(file);
+        // $scope.content = 'http://banistmo-backend.herokuapp.com/' + fileURL;
+
+        $scope.content = $sce.trustAsResourceUrl(fileURL);
+        console.log('$scope.content.. : ' + $scope.content);
+          console.log('$scope.categories : ' +  angular.toJson($scope.circulars, ' ') );
+      }
+    }).catch(function(error) {
+        var error = JSON.parse(error);
+        IonicPopupService.alert("ERROR!!", error.msg);
+        console.log('error : ' + angular.toJson(error, ' '));
+    });
+
+  });
 });
--- www/js/services/CouponService.js
+++ www/js/services/CouponService.js
@@ -98,4 +98,36 @@ angular.module('starter')
 
         return def.promise;
     };
+
+    this.getAllCirculars = function() {
+        var def = $q.defer();
+
+        var url = AppSettings.url + 'circulars';
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        $http({
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            transformRequest: function(obj) {
+                var str = [];
+                for (var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            }
+        }).success(function(body, status, headers, config) {
+            // console.log("body : " + JSON.stringify(body));
+            def.resolve(body);
+        }).
+        error(function(error, status, headers, config) {
+            console.log("Error : " + JSON.stringify(error));
+            def.reject(JSON.stringify(error));
+        });
+
+        return def.promise;
+    };
 }]);
